5081f891c022b247b4bbf5387af25864e1229076
Yash Mathur <yoshi.mathur@gmail.com>
2023-01-16 20:06:50 -0500

    Add files via upload

--- Scrapers/extractPosts.py
+++ Scrapers/extractPosts.py
@@ -16,12 +16,71 @@ from selenium.webdriver.chrome.options import Options
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+username = ""
+password = ""
+
+# db setup
+cred = credentials.Certificate("/Users/yashmathur/Downloads/sprout-akcyl333-firebase-adminsdk-4bq7t-5594ca04c8.json")
+app = firebase_admin.initialize_app(cred)
+firestore_client = firestore.client()
+db = firestore_client.collection("insta")
+
 # driver setup
 driver = webdriver.Chrome(ChromeDriverManager().install())
 actions = ActionChains(driver)
+driver.get("https://www.instagram.com")
+driver.maximize_window()
+time.sleep(10)
+
+# function to check if element exists
+def doesExist(xpath):
+    try:
+        driver.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+# log in 
+inputs = driver.find_elements(By.TAG_NAME, "input")
+for input in inputs:
+    if input.get_attribute("aria-label") == "Phone number, username, or email":
+        print("Entering username...")
+        input.send_keys(username)
+        print("Entered username...")
+
+    if input.get_attribute("aria-label") == "Password":
+        print("Entering password...")
+        input.send_keys(password)
+        print("Entered password...")
+        break
+
+print("Clicking log in...")
+button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+button.click()
+print("Clicked log in ...")
+time.sleep(10)
+
+# clearing info dialog
+print("Clearing dialog...")
+buttons = driver.find_elements(By.TAG_NAME, "button")
+for button in buttons:
+    if button.get_attribute("innerHTML").strip() == "Not Now":
+        button.click()
+        print("Cleared dialog...")
+        time.sleep(2)
+        break
+
+# clearing notification dialog
+buttons = driver.find_elements(By.TAG_NAME, "button")
+for button in buttons:
+    if button.get_attribute("innerHTML").strip() == "Not Now":
+        button.click()
+        print("Cleared dialog...")
+        time.sleep(2)
+        break
 
 # getting saved user posts
-name = "kendalljenner"
+name = "victoriaparis"
 docs = db.where("user", "==", name).stream()
 
 postLinks = set([])
@@ -42,13 +101,14 @@ def doesExist(xpath):
     return True
 
 counter = 0
+skipped = set([])
 # grab data from each post 
 for postLink in postLinks: 
     counter = counter + 1
     # opening post
     driver.get(postLink)
     driver.maximize_window()
-    time.sleep(10)
+    time.sleep(5)
 
     # creating docID 
     split = postLink.split("/")
@@ -165,6 +225,9 @@ for postLink in postLinks:
         db.document(docID).update(data)
     else: 
         # post not found? 
-        db.document(docID).delete()
+        print("No media found...")
+        skipped.add(docID)
+
+print(skipped)
 
 print("Completed Data Extraction on " + str(counter) + " posts!")
